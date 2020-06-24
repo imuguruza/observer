937c50afde2122829173ca3146acc0a34d7b505c
imuguruza <inigo.muguruza89@gmail.com>
2020-06-24 20:24:09 +0200

    Enable back SPI in alhambra design

--- rtl/observer_alhambra.v
+++ rtl/observer_alhambra.v
@@ -3,11 +3,10 @@ module observer_alhambra
 (
  input wire  i_clk,
  input wire  i_user_btn,
- // No SPI for now
- // output wire spi_g_sen_sclk,
- // output wire spi_g_sen_cs_n,
- // output wire spi_g_sen_mosi,
- // input wire  spi_g_sen_miso,
+ output wire spi_g_sen_sclk,
+ output wire spi_g_sen_cs_n,
+ output wire spi_g_sen_mosi,
+ input wire  spi_g_sen_miso,
  output wire q,
  output wire o_uart_tx);
 
@@ -25,10 +24,10 @@ module observer_alhambra
      (.i_clk (wb_clk),
       .i_rst (wb_rst),
       .i_user_btn (i_user_btn),
-      // .o_sclk   (spi_g_sen_sclk),
-      // .o_cs_n   (spi_g_sen_cs_n),
-      // .o_mosi   (spi_g_sen_mosi),
-      // .i_miso   (spi_g_sen_miso),
+      .o_sclk   (spi_g_sen_sclk),
+      .o_cs_n   (spi_g_sen_cs_n),
+      .o_mosi   (spi_g_sen_mosi),
+      .i_miso   (spi_g_sen_miso),
       .o_uart_tx  (q));
 
 endmodule
